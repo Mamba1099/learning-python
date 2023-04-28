0272a735c26341440aab70e2917e07d9a2ede572
Mamba1099 <mambakaranja240@gmail.com>
2023-04-28 22:35:33 +0300

    learning python functions

---
+++ functions/basics.py
@@ -0,0 +1,4 @@
+def my_function(first_name,second_name,third_name):
+    print(first_name + " " +second_name + " " +third_name)
+
+my_function("sammy", "karanja", "kingangi")
---
+++ functions/bio.py
@@ -0,0 +1,19 @@
+def my_name(fname,sname):
+    print("My name is" + " " + fname +" " + sname)
+my_name("sammy", "karanja")
+
+
+def my_colour(*colours):
+    print("My favorite color is" + " " + colours[2])
+my_colour("Blue", "Green", "White", "Orange")
+
+
+def my_country(city,country):
+    print("Am from" +" " + city +" " +country)
+my_country("nairobi", "Kenya")
+
+
+def my_siblings(*siblings):
+    print("My favorite is" + " " + siblings[2])
+my_siblings("jemie", "ann", "Tracy")
+
