3a52600c62c807e55a70545e736f44e2beef51cb
Mamba1099 <mambakaranja240@gmail.com>
2023-04-29 22:47:51 +0300

    learning python lists

---
+++ python-list/list.py
@@ -0,0 +1,26 @@
+this_list=["apple", "banana", "mango"]
+print(len(this_list))
+print(this_list)
+
+list1=["apple", "mango", "banana"]
+print(list1)
+print(len(list1))
+
+
+list2=[1,2,3,4,5]
+print(list2)
+print(len(list2))
+
+
+list3=[True,False,True]
+print(list3)
+print(len(list3))
+
+
+this_list=["male", 13, "abc"]
+print(this_list)
+
+
+this_list=["apple", "banana", "mango"]
+print(type(this_list))
+
