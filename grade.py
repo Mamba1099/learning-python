6d1a8a5b9fef83ae928efbee0b07b187847c1750
Mamba1099 <mambakaranja240@gmail.com>
2023-05-02 13:50:29 +0300

    checking grades

---
+++ grade.py
@@ -0,0 +1,14 @@
+score = int(input("score: "))
+
+if score >= 90 and score <= 100:
+    print("Grade: A" + " " + "pass")
+
+elif score >= 80 and score <=100:
+    print("Grade: B" + " " + "pass")
+
+elif score >= 70 and score <= 100:
+    print("Grade: C " + " " + "average")
+
+else: 
+    print("Grade: F" + " " + "fail")
+            
