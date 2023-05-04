b902f48ce093c714ef027ed13f58d2b6a6ef263a
Mamba1099 <mambakaranja240@gmail.com>
2023-05-04 15:53:36 +0300

    lists of dictionary

---
+++ hogwart.py
@@ -0,0 +1,8 @@
+students = {
+    "Hermionese":"Gryffindor",
+    "Harry":"Gryffindor",
+    "Ron":"Gryffindor",
+    "Draco":"Slytherin",
+}
+for student in students:
+    print(student, students[student], sep=", ")
