d5392c55190f1970780eedd70bbaf261aaa201af
Mamba1099 <mambakaranja240@gmail.com>
2023-05-04 15:43:17 +0300

    dictionaries

---
+++ hogwarts.py
@@ -0,0 +1,11 @@
+students = {
+    "Hermionese":"Gryffindor",
+    "Harry":"Gryffindor",
+    "Ron":"Gryffindor",
+    "Draco":"Slytherin",
+}
+
+print(students["Hermionese"])
+print(students["Harry"])
+print(students["Ron"])
+print(students["Draco"])
