f1a00cee63c328d8730ca07ebc6b7932505cead2
Mamba1099 <mambakaranja240@gmail.com>
2023-05-09 12:56:59 +0300

    checking profit or loss

---
+++ profit.py
@@ -0,0 +1,23 @@
+selling_price =  int(input("Enter the selling price: "))
+buying_price =int(input("Enter the buying price: "))     
+
+
+profit = selling_price  - buying_price
+percentage_profit = ((selling_price - buying_price) * 100) / selling_price
+loss = buying_price - selling_price
+percentage_loss = ((buying_price - selling_price) * 100) / selling_price
+
+    
+
+
+if selling_price > buying_price:
+  print("profit = ", + profit)
+  print("percentage profit = ", + percentage_profit)
+else:
+ print("loss = ", + loss)
+ print("percentage loss = ", + percentage_loss)
+
+
+
+
+
