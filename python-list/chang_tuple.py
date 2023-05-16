fd3871a4863874c5d5b911799f5b5950ac532c56
Mamba1099 <mambakaranja240@gmail.com>
2023-05-16 22:41:04 +0300

    python tuples

--- python-list/chang_tuple.py
+++ python-list/chang_tuple.py
@@ -1,5 +1,20 @@
 this_list = ("mango", "apple", "cherry", "banana")
 y = list(this_list)
-y[2] = "strawberry"
+y.append("strawberry")
+this_list = tuple(y)
+print(this_list)
+
+
+# add tuple to a tuple
+this_list = ("blue", "white", "orange")
+y = ("green", "purple")
+this_list += y
+print(this_list)
+
+
+# remove item in a tuple
+this_list = ("cow", "goat", "cat")
+y = list(this_list)
+y.remove("goat")
 this_list = tuple(y)
 print(this_list)
