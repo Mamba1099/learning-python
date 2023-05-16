842009b1711bf347d09d0081619206c80728f67f
Mamba1099 <mambakaranja240@gmail.com>
2023-05-17 00:48:08 +0300

    str function in class and objects

---
+++ python-list/str_function.py
@@ -0,0 +1,11 @@
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+def my_function(self):
+  print("My name is", + self.name)
+  print("I am", + self.age)
+
+p1 = person("kevin", 56)
+print(p1)      
