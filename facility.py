881c5f8cf96311395526851ac47b03bf234737d6
sammy karanja <mambakaranja240@gmail.com>
2023-07-12 08:06:26 -0700

    task: introduced threading to code

--- facility.py
+++ facility.py
@@ -1,10 +1,30 @@
-monthly_salary = input ("Enter the monthly salary:")
-years_experienced = input ("Enter the years experienced:")
-years_salary = sum = 12 * int(monthly_salary)
-print(sum)
+import threading
 
-if int(years_salary) > 300000 and int(years_experienced) > 2:
-    print("you qualify for the facility...")
 
-else:
-    print("you dont qualify for the facility...")
+monthly_salary = input("Enter the monthly salary:")
+years_experienced = input("Enter the years experienced:")
+
+
+def my_calculations():  
+    years_salary = 12 * int(monthly_salary)
+    print(years_salary)
+
+
+    if int(years_salary) > 300000 and int(years_experienced) > 2:
+         print("you qualify for the facility...")
+
+    else:
+         print("you dont qualify for the facility...")
+
+
+my_calculations()
+
+
+def my_thread():
+    thread_1 = threading.Thread(target = my_calculations)
+
+    thread_1.start()
+
+my_thread()
+
+
