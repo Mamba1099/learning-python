4189659f16660ea8c89aa9b873451d91461f12ec
sammy karanja <mambakaranja240@gmail.com>
2023-07-13 00:32:46 -0700

    facility status check

--- facility.py
+++ facility.py
@@ -1,30 +1,19 @@
-import threading
-
 
 monthly_salary = input("Enter the monthly salary:")
 years_experienced = input("Enter the years experienced:")
+years_salary = 0.00
 
 
 def my_calculations():  
-    years_salary = 12 * int(monthly_salary)
-    print(years_salary)
+    years_salary=  12 * int(monthly_salary)
+    return(years_salary)
+my_calculations()
 
-
+def my_statement():
     if int(years_salary) > 300000 and int(years_experienced) > 2:
          print("you qualify for the facility...")
 
     else:
          print("you dont qualify for the facility...")
-
-
-my_calculations()
-
-
-def my_thread():
-    thread_1 = threading.Thread(target = my_calculations)
-
-    thread_1.start()
-
-my_thread()
-
+my_statement()
 
