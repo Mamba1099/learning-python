27e89e244aee391235661d3262271ef11ee388f2
sammy karanja <mambakaranja240@gmail.com>
2023-07-13 00:54:15 -0700

    checking business profit

--- profit.py
+++ profit.py
@@ -7,16 +7,15 @@ percentage_profit = ((selling_price - buying_price) * 100) / selling_price
 loss = buying_price - selling_price
 percentage_loss = ((buying_price - selling_price) * 100) / selling_price
 
-    
+def my_profit():
+  if selling_price > buying_price:
+    print("profit = ", + profit)
+    print("percentage profit = ", + percentage_profit)
+  else:
+    print("loss = ", + loss)
+    print("percentage loss = ", + percentage_loss)
 
-
-if selling_price > buying_price:
-  print("profit = ", + profit)
-  print("percentage profit = ", + percentage_profit)
-else:
- print("loss = ", + loss)
- print("percentage loss = ", + percentage_loss)
-
+my_profit()
 
 
 
